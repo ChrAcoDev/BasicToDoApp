bc6f43cf38ab9b1be5125a38c5281adc1aacad4b
Christian Acosta <Christian.Acosta.Developer@gmail.com>
2022-12-22 11:42:23 -0500

    Updated Todo type for dates

--- src/classes/Todo.ts
+++ src/classes/Todo.ts
@@ -2,11 +2,14 @@ class Todo {
     id: number;
     text: string;
     isCompleted: boolean;
+    dateAdded: Date;
+    dateCompleted?: Date;
 
     constructor(t: string) {
         this.text = t;
         this.id = Math.floor(Math.random() * 100000);
         this.isCompleted = false;
+        this.dateAdded = new Date();
     }
 }
 
